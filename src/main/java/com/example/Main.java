fbdb3a34cdbb320a6b269956f3068e3b18a93a97
Guilherme23645 <gui.depaula236@gmail.com>
2025-08-15 23:37:59 -0300

    feat: display name

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 public class Main {
 	public static void main(String[] args) {
 		Person p = new Person("Alice",19);
-		System.out.println(p);
+		System.out.println(p.getName());
 	}
 }
